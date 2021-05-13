fbb1631417c8cd70c1d9c7b01eb3c2fa94d200b7
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 14:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+var element = document.createElement('div');
+document.body.appendChild(element);
+
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++)
+    {
+    let li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);
+    }
+
+ element.appendChild(ul);
+
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "LUKKAAAA is the champion";
+// innerText = document.createTextNode("LUKAAAAA is the champion");
+// newHeader.appendChild(innerText);
+document.body.appendChild(newHeader);
